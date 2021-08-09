fd6bf7fad7758ba0f2871a55fc63226485649e7c
emrecanduygulu <58653877+emrecanduygulu@users.noreply.github.com>
2021-08-10 00:27:31 +0300

    canScore function has been created.

--- optionalToplantı.playground/Contents.swift
+++ optionalToplantı.playground/Contents.swift
@@ -8,9 +8,10 @@ struct FootballTeam {
     let country: String?
     let colors: (primary: String?, secondary: String?)
     let stadium: String?
-    var score: Int?
-    
 
+    var starters: [Player] {
+        players.filter() { player -> Bool in player.injuryReport == nil }
+    }
 }
 
 struct Player {
@@ -21,8 +22,12 @@ struct Player {
     var jerseyNumber: Int
     var injuryReport: String?
     var position: PositionType
-   // let finishing: Int
     let heading: Int
+    let finishing: Int
+    
+    var fullName: String {
+        return name + " " + surname
+    }
     
     
     enum PositionType {
@@ -33,80 +38,116 @@ struct Player {
     }
 }
 
-//struct PlayerStore {
-//
-//    let player1: Player = Player.init(name: "Alex de", surname: "Souza", id: 01, jerseyNumber: 10,
-//                                      position: Player.PositionType.midfielder)
-//    let player2: Player = Player.init(name: "Roberto", surname: "Carlos", id: 02, jerseyNumber: 3,
-//                                        position: Player.PositionType.defender)
-//    let player3: Player = Player.init(name: "Volkan", surname: "Demirel", id: 03, jerseyNumber: 1,
-//                                        position: Player.PositionType.goalKeeper)
-//    let player4: Player = Player.init(name: "Semih", surname: "Şentürk", id: 04, jerseyNumber: 11,
-//                                        position: Player.PositionType.forward)
-//    let player5: Player = Player.init(name: "Goerge", surname: "Hagi", id: 05, jerseyNumber: 10,
-//                                        position: Player.PositionType.midfielder)
-//    let player6: Player = Player.init(name: "Mondra", surname: "Mondragon", id: 06, jerseyNumber: 1,
-//                                        position: Player.PositionType.goalKeeper)
-//    let player7: Player = Player.init(name: "Sabri", surname: "Sarıoğlu", id: 07, jerseyNumber: 55,
-//                                        position: Player.PositionType.defender)
-//    let player8: Player = Player.init(name: "Johan", surname: "Elmander", id: 08, jerseyNumber: 11,
-//                                        position: Player.PositionType.forward)
-//    let player9: Player = Player.init(name: "Radamel", surname: "Falcao", id: 09, jerseyNumber: 99,
-//                                      injuryReport: "Foot", position: Player.PositionType.forward)
-//    let player10: Player = Player.init(name: "Robin van", surname: "Persie", id: 10, jerseyNumber: 99,
-//                                       injuryReport: "Head", position: Player.PositionType.forward)
-//}
-
-
 let player1: Player = Player.init(name: "Alex de", surname: "Souza", id: 01, jerseyNumber: 10,
-                                  position: Player.PositionType.midfielder, heading: 75)
+                                  position: Player.PositionType.midfielder, heading: 75, finishing: 85)
 let player2: Player = Player.init(name: "Roberto", surname: "Carlos", id: 02, jerseyNumber: 3,
-                                  position: Player.PositionType.defender, heading: 50)
+                                  position: Player.PositionType.defender, heading: 50, finishing: 75)
 let player3: Player = Player.init(name: "Volkan", surname: "Demirel", id: 03, jerseyNumber: 1,
-                                  position: Player.PositionType.goalKeeper, heading: 10)
+                                  position: Player.PositionType.goalKeeper, heading: 10, finishing: 1)
 let player4: Player = Player.init(name: "Semih", surname: "Şentürk", id: 04, jerseyNumber: 11,
-                                  position: Player.PositionType.forward, heading: 86)
+                                  position: Player.PositionType.forward, heading: 86, finishing: 77)
 let player5: Player = Player.init(name: "Goerge", surname: "Hagi", id: 05, jerseyNumber: 10,
-                                  position: Player.PositionType.midfielder, heading: 75)
+                                  position: Player.PositionType.midfielder, heading: 75, finishing: 85)
 let player6: Player = Player.init(name: "Mondra", surname: "Mondragon", id: 06, jerseyNumber: 1,
-                                  position: Player.PositionType.goalKeeper, heading: 10)
+                                  position: Player.PositionType.goalKeeper, heading: 10, finishing: 1)
 let player7: Player = Player.init(name: "Sabri", surname: "Sarıoğlu", id: 07, jerseyNumber: 55,
-                                  position: Player.PositionType.defender, heading: 35)
+                                  position: Player.PositionType.defender, heading: 35, finishing: 55)
 let player8: Player = Player.init(name: "Johan", surname: "Elmander", id: 08, jerseyNumber: 11,
-                                  position: Player.PositionType.forward, heading: 90)
+                                  position: Player.PositionType.forward, heading: 90, finishing: 80)
 let player9: Player = Player.init(name: "Radamel", surname: "Falcao", id: 09, jerseyNumber: 99,
-                                  injuryReport: "Foot", position: Player.PositionType.forward, heading: 80)
+                                  injuryReport: "Foot", position: Player.PositionType.forward, heading: 80, finishing: 88)
 let player10: Player = Player.init(name: "Robin van", surname: "Persie", id: 10, jerseyNumber: 99,
-                                   injuryReport: "Head", position: Player.PositionType.forward, heading: 90)
+                                   injuryReport: "Head", position: Player.PositionType.forward, heading: 90, finishing: 85)
 
 let Fenerbahçe: FootballTeam = FootballTeam(players: [player1, player2, player3, player4, player10], name:"Fenerbahçe" , country: "İstanbul", colors: (primary: "Yellow", secondary: "Blue"), stadium: "Şükrü Saraçoğlu")
 
 let Galatasaray: FootballTeam = FootballTeam(players: [player5, player6, player7, player8, player9], name:"Galatasaray" , country: "İstanbul", colors: (primary: "Yellow", secondary: "Red"), stadium: "TT Arena")
 
-//func canScore(finishing: Int) -> Bool {
-//    return true
-//}
 
 func squadAnnouncement(squadOne: FootballTeam, squadTwo: FootballTeam) {
     
     print("We have an Istanbul Derby today. \(squadOne.name) will face against \(squadTwo.name)")
-    print("\(squadOne.name)'s starting eleven is \(squadOne.players.map{$0.name})")
-    print("\(squadTwo.name)'s starting eleven is \(squadTwo.players.map{$0.name})")
+    print("\(squadOne.name)'s starting eleven is  \(squadOne.starters.map{$0.fullName}.joined(separator: "\n"))")
+    print("\(squadTwo.name)'s starting eleven is \(squadTwo.starters.map{$0.fullName}.joined(separator: "\n"))")
     
 }
 
 squadAnnouncement(squadOne: Fenerbahçe, squadTwo: Galatasaray)
 
 
+var fenerbahceScore = 0
+var galatasarayScore = 0
+
+enum BoxField: CaseIterable {
+    case leftWing 
+    case rightWing
+    case center
+    case outside
+    
+    var expectedGoals: Double {
+        switch self {
+        case .leftWing:
+            return 0.60
+        case .rightWing:
+            return 0.60
+        case .center:
+            return 0.70
+        case .outside:
+            return 0.50
+            }
+        }
+    var fieldNames: String {
+        switch self {
+        case .leftWing:
+            return "left wing"
+        case .rightWing:
+            return "right wing"
+        case .center:
+            return "center"
+        case .outside:
+            return "outside"
+        }
+    }
+}
+
+func canScore (team: FootballTeam) {
+    let goalScorer = team.starters.randomElement()
+    guard let scorer = goalScorer else { return }
+    guard let randomField = BoxField.allCases.randomElement() else { return }
+    let goalPossibility = Double(scorer.finishing) * randomField.expectedGoals
+    if goalPossibility >= 40 {
+        if team.name == "Fenerbahçe" {
+            fenerbahceScore += 1
+        }else {
+            galatasarayScore += 1
+        }
+        print("\(scorer.fullName) scored from the \(randomField.fieldNames). Fenerbahçe \(fenerbahceScore) - Galatasaray \(galatasarayScore)")
+    } else { print("\(scorer.fullName) took the shot from the \(randomField.fieldNames) but it didn't went in.")
+        
+    }
+}
+
 func cornerGoal(team: FootballTeam) {
-    var teamScore = team.score ?? 0
-    let goalScorer = team.players.randomElement()
-    if goalScorer!.heading >= 85 {
-        teamScore += 1
-        print ("\(team) will be using corner kick. \(goalScorer!) has scored an absolute header!")
+    let goalScorer = team.starters.randomElement()
+    guard let scorer = goalScorer else { return }
+    if scorer.heading >= 80 {
+        if team.name == "Fenerbahçe" {
+            fenerbahceScore += 1
+        }else {
+            galatasarayScore += 1
+        }
+        print ("\(team.name) will be using corner kick. \(scorer.fullName) has scored an absolute header! Fenerbahçe \(fenerbahceScore) - Galatasaray \(galatasarayScore)")
     } else {
-        print("Not Goal")
+        print("\(scorer.fullName) jumped really well but he couldn't score")
     }
 }
 
 cornerGoal(team: Fenerbahçe)
+cornerGoal(team: Galatasaray)
+canScore(team: Fenerbahçe)
+canScore(team: Galatasaray)
+print(fenerbahceScore)
+print(galatasarayScore)
+
+
+
